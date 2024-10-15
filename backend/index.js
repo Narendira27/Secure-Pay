ab84c5567fb14f09e4f98aa814b80e4458c7e085
Narendira27 <41791017+Narendira27@users.noreply.github.com>
2024-10-15 16:29:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,14 @@ const corsOptions = {
     origin: ['https://securepay-frontend.onrender.com', 'https://talks-mines-skin-licence.trycloudflare.com', 'http://localhost:5173'],
 };
 
+
+
 app.use(cors(corsOptions))
 app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send({ status: "ok", success: true });
+});
 
 app.use('/api/v1', rootRouter)
 
