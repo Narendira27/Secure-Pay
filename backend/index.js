fb9f15ad6fae5aa0fcf3aae08536c26f8bddc167
Narendira27 <41791017+Narendira27@users.noreply.github.com>
2024-12-08 00:13:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,9 @@ const rootRouter = require('./routes/index.js')
 
 const app = express()
 
-const corsOptions = {
-    origin: ['https://securepay-frontend.onrender.com', 'https://talks-mines-skin-licence.trycloudflare.com', 'http://localhost:5173'],
-};
 
 
-
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
